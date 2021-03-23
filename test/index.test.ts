3e8a1eb156bd4e6b5d2d9349c6b8f7c68a3ebc8a
driverjb <driverjb09@gmail.com>
2021-03-23 10:34:13 -0400

    execute the sqlite vault tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,9 @@
 import { expect } from 'chai';
 import 'mocha';
 import FileVaultTests from './FileVault.test';
+import SQLiteVaultTests from './SQLiteVault.test';
 
 describe('Vault Tests', () => {
   FileVaultTests();
+  SQLiteVaultTests();
 });
