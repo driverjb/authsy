d23df993e5ad967c846d12bd8c0ffe064b3464ff
driverjb <driverjb09@gmail.com>
2021-03-24 09:59:34 -0400

    reduced to strings for more universal use

--- src/util/errors.module.ts
+++ src/util/errors.module.ts
@@ -1,4 +1,7 @@
-export const cannotDelete = new Error(`User id does not exist. Cannot delete entry.`);
-export const cannotUpdate = new Error(`User id does not exist. Cannot update password hash.`);
+export const cannotDelete = `User id does not exist. Cannot delete entry.`;
+export const cannotUpdate = `User id does not exist. Cannot update password hash.`;
+export const multiChangeError = (total: number, action: string) =>
+  `Expected to ${action} 1 user, but detected ${total} changes.`;
+export const failedCreate = `Failed to create new user entry`;
 
-export default { cannotDelete, cannotUpdate };
+export default { cannotDelete, cannotUpdate, multiChangeError, failedCreate };
