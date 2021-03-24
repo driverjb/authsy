4ffa1d79111c678795aee6e9c2f50a011a6e9ec4
driverjb <driverjb09@gmail.com>
2021-03-24 10:02:43 -0400

    build new version

--- lib/util/errors.module.d.ts
+++ lib/util/errors.module.d.ts
@@ -1,7 +1,11 @@
-export declare const cannotDelete: Error;
-export declare const cannotUpdate: Error;
+export declare const cannotDelete = "User id does not exist. Cannot delete entry.";
+export declare const cannotUpdate = "User id does not exist. Cannot update password hash.";
+export declare const multiChangeError: (total: number, action: string) => string;
+export declare const failedCreate = "Failed to create new user entry";
 declare const _default: {
-    cannotDelete: Error;
-    cannotUpdate: Error;
+    cannotDelete: string;
+    cannotUpdate: string;
+    multiChangeError: (total: number, action: string) => string;
+    failedCreate: string;
 };
 export default _default;
